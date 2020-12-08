cba12a067f6ff398210a210fffd75c580037cf99
314ga <jakub.piga.cp@gmail.com>
2020-12-08 20:46:11 +0100

    Created layout for API calls added DB dependencies

--- ManufacturerAPI.cs
+++ ManufacturerAPI.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ManufacturerFUnction
 {
@@ -19,17 +21,73 @@ namespace ManufacturerFUnction
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
+            string userRequest = req.Query["requestBody"];
+            string responseMessage = "";
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            userRequest = userRequest ?? data?.requestBody;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            if(!string.IsNullOrEmpty(userRequest))
+            {
+                var str = Environment.GetEnvironmentVariable("sqldb_connection");
+                using (SqlConnection conn = new SqlConnection(str))
+                {
+                    var text = "";
+                    conn.Open();
+                    switch(userRequest)
+                    {
+                        case "manufacturers":
+                        {
+                            text = "SELECT * from weather where origin = 'JFK' FOR JSON PATH; ";
+                            break;
+                        }
+                        case "manufacturers-flights":
+                        {
+                             text = "SELECT * from weather where origin = 'JFK' FOR JSON PATH; ";
+                            break;
+                        }
+                        case "airbus-models":
+                        {
+                            text = "SELECT * from weather where origin = 'JFK' FOR JSON PATH; ";
+                            break;
+                        }
+                        default:
+                        {
+                            text = "error";
+                            break;
+                        }
+                        
+                    }
+                    if(text != "error" || text != "")
+                    {
+                        using (SqlCommand cmd = new SqlCommand(text, conn))
+                        {
+                            SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                            // Execute the command and log the # rows affected.
+                            while (reader.Read())
+                            {
+                                IDataRecord result = (IDataRecord)reader;
+                                responseMessage += String.Format("{0},", result[0]);
+                            }
+                            // Call Close when done reading.
+                            reader.Close();
 
-            return new OkObjectResult(responseMessage);
+                        }
+                    }
+                    else 
+                    {
+                        return new NotFoundObjectResult(userRequest);
+                    }
+                    
+                }
+                return new OkObjectResult(responseMessage);
+            }
+            else
+            {
+                return new NotFoundObjectResult(userRequest);
+            }
+            // Get the connection string from app settings and use it to create a connection.
+           
         }
     }
 }
